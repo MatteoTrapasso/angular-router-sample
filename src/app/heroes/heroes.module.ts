7ad3ea3263f3bec35ef0dbe5edb61a70d96f117a
MatteoTrapasso <matteo.trapasso97@gmail.com>
2021-02-03 14:40:04 +0100

    error in  heroes$ && selectedId

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
 
@@ -12,7 +14,8 @@ import { HeroesRoutingModule } from './heroes-routing.module';
     HeroesRoutingModule
   ],
   declarations: [
-
+    HeroListComponent,
+    HeroDetailComponent
   ]
 })
 export class HeroesModule {}
